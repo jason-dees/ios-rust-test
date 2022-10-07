f43b0975739b3f39f56ded07b706de45dadfe8fd
Jason Dees <jhdees@gmail.com>
2022-10-07 12:51:43 -0400

    Initial Commit

---
+++ ios-rust/ios_rustApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_rustApp.swift
+//  ios-rust
+//
+//  Created by Jason Dees on 10/7/22.
+//
+
+import SwiftUI
+
+@main
+struct ios_rustApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
