dc0306ca8fcb82baea61649e77ed695bca83849a
Jason Dees <jhdees@gmail.com>
2022-10-08 10:01:31 -0400

    adding rust building process

--- ios-rust/ContentView.swift
+++ ios-rust/ContentView.swift
@@ -13,10 +13,17 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("\(sayHello(to: "World"))")
         }
         .padding()
     }
+    
+    func sayHello(to: String) -> String {
+            let result = rust_greeting(to)
+            let swift_result = String(cString: result!)
+            rust_greeting_free(UnsafeMutablePointer(mutating: result))
+            return swift_result
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ ios-rust/Greetings-Bridge-Header.h
@@ -0,0 +1,13 @@
+//
+//  Greetings-Bridg-Header.h
+//  ios-rust
+//
+//  Created by Jason Dees on 10/7/22.
+//
+
+#ifndef Greetings_Bridge_Header_h
+#define Greetings_Bridge_Header_h
+
+#import "greetings.h"
+
+#endif /* Greetings_Bridg_Header_h */
